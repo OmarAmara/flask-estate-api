d1b130e2d66a8e1924b2d1fc3863c31782c50db4
Omar Amara <O_amara@hotmail.com>
2020-02-17 17:34:34 -0600

    implemented login_manager, applying to routes

--- app.py
+++ app.py
@@ -3,11 +3,12 @@
 # g(global variable)
 from flask import Flask, g, jsonify
 
-# try destructuring here or importing specific variables
+from flask_login import LoginManager
 
+# try destructuring here or importing specific variables
 from resources.users import users
 
-from models import DATABASE, User
+from models import DATABASE, User, DoesNotExist
 
 
 DEBUG = True
@@ -17,6 +18,25 @@ PORT = 8000 # hidr in production into .env
 
 app = Flask(__name__)
 
+
+##### secret key: Store as environment variable, .gitignore when deploying
+app.secret_key = 'Super secret, nobody will ever guess it!'
+
+# instantiate
+login_manager = LoginManager()
+# connect app
+login_manager.init_app(app)
+
+# cookie created with login_user as can be seen in register/ login users routes
+@login_manager.user_loader
+def load_user(userid):
+	try:
+		return User.get(User.id == userid)
+	except DoesNotExist:
+		return None
+
+
+### CORS will be inserted here to enable access to API ### Will also need to be installed.
 
 
 # blueprint to set 'controllers' to handle model routes
