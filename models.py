2ceaec040d04b23e54bb8189800861baaf37f059
Omar Amara <O_amara@hotmail.com>
2020-02-16 21:49:11 -0600

    Completed DB connection, class and table set-up in progress

--- models.py
+++ models.py
@@ -1,2 +1,31 @@
 ## modules
 from peewee import *
+
+
+
+
+
+#### .gitignore DB connection during deployment, transfer to psql
+# portable data for development: value = DB connection string
+DATABASE = SqliteDatabase('searches.sqlite')
+
+
+
+## class models
+
+	# insert class models here
+
+## class models
+
+
+# method to set-up DB's connection
+def initialize():
+	DATABASE.connect()
+
+
+# creates tables for classes above
+# DATABASE.create_tables([ ], safe=True)
+print('Connected to DB, created tables if non-existed')
+
+# close DB connection, free space in connection pool
+DATABASE.close()
