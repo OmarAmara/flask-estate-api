c2a2e1802a9301db37428cad662fe10af00a04d0
Omar Amara <O_amara@hotmail.com>
2020-02-17 22:39:01 -0600

    User able to create new search

--- resources/searches.py
+++ resources/searches.py
@@ -7,8 +7,50 @@ from flask_login import current_user, login_required
 
 from playhouse.shortcuts import model_to_dict
 
+# searches Blueprint/ 'controller'
 searches = Blueprint('searches', 'searches')
 
 
+#### create routes, utilize login_required: index, delete, update
 
-#### create routes, utilize login_required
+# search list index
+# @searches.route('/', methods=['GET'])
+# # route only available if logged in
+# @login_required
+# def searches_index():
+	# INSERT QUERY AND RETURN LOGIC
+
+
+# search create route
+@searches.route('/', methods=['POST'])
+@login_required
+def create_search():
+	payload = request.get_json()
+
+	search = Search.create(
+		name=payload['name'],
+		zipcode=payload['zipcode'],
+		sqrft=payload['sqrft'],
+		upperprice=payload['upperprice'],
+		lowerprice=payload['lowerprice'],
+		client=current_user.id
+	)
+
+	search_dict = model_to_dict(search)
+
+	search_dict['client'].pop('password')
+	search_dict['client'].pop('secretanswer')
+	search_dict['client'].pop('secretquestion')
+
+	return jsonify(
+		data=search_dict,
+		message='Successfully created Search!',
+		status=201
+	), 201
+
+### route to delete
+
+
+
+
+
